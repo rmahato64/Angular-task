a559193b830f67b52889e4ba3c60ab159c9521c7
Rakesh Mahato <rmahato64@gmail.com>
2021-12-25 09:34:44 +0200

    Update app component test case
    Refs None

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'orders'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('orders');
+    expect(app.title).toEqual('Orders');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('orders app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Orders');
   });
 });
